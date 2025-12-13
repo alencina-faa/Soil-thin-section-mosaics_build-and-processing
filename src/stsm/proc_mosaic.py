Fix convex shape calculation in proc_cont_great_50
Corrects the formula for convex shape by using area in the numerator and the sum of convex hull perimeters in the denominator, as per the referenced equation. This ensures accurate quantification of convexity.

src/stsm/proc_mosaic.py
@@ -294,8 +294,8 @@ def proc_cont_great_50(self):
             #(Corrected) Convex Shape = 4 pi area / Convex_perimeter^2 
             # adapted from Ringrose-Voase 1991, Micromorphology of Soil Structure:
             #Description, Quantification, Application, Eq. 2
-            C := float(4 * m.pi * (cv2.arcLength(cv2.convexHull(cont[2]), True) + 
-                             np.sum([cv2.arcLength(cv2.convexHull(contch), True) for contch in cont[3]]))/cont[5]**2), 
+            C := float(4 * m.pi * cont[4] / (cv2.arcLength(cv2.convexHull(cont[2]), True) + 
+                             np.sum([cv2.arcLength(cv2.convexHull(contch), True) for contch in cont[3]]))**2), 
             m.sqrt(S**2 + C**2)/2 , #Pore elongation
             m.atan(S/C)*180/m.pi, #Pore irragularity (deg)
             2 * m.sqrt(cont[4] / m.pi) / self.calibration, #Equivalent diameter = 2 sqrt(area / pi) / self.calibration